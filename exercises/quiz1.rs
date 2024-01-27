7f1f32b3bd0d8afdbde3f4119e4707e52c4d105c
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-01-27 18:40:35 -0300

    Add exercises resolutions so far

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,10 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
-// Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    let multiplier = if apples > 40 { 1 } else { 2 };
+    apples * multiplier
+}
 
 // Don't modify this function!
 #[test]
