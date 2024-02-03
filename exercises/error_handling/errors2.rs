0c66d46c3d699eafa65b998490e8bdd309797bc1
Ezequiel Torres Feyuk <ezequiel.torresfeyuk@gmail.com>
2024-02-03 11:07:49 -0300

    More exercises resolved

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,14 +19,20 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty = match item_quantity.parse::<i32>() {
+        Ok(qty) => qty,
+        Err(error) => return Err(error),
+    };
+    // Shorter and nicer solution. I leave the match expression here to not forget
+    // that the ? is a shortcut of the macro ?. Remember that if the function return
+    // value does not match with the function (in this case string::parse) then we cannot
+    // use the ? macro and the match expression will need to be used
+    //let qty = item_quantity.parse::<i32>()?;
 
     Ok(qty * cost_per_item + processing_fee)
 }
